58fc7ee57b34aa89bc1825537392994a78d16ec2
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2024-01-03 02:10:45 -0500

    Add support for old DNSRegistrar

--- ponder.config.ts
+++ ponder.config.ts
@@ -1,16 +1,23 @@
-import type { PonderConfig } from "@ponder/core";
+import type { PonderConfig } from '@ponder/core'
 
 export const config: PonderConfig = {
   networks: [
-    { name: "mainnet", chainId: 1, rpcUrl: process.env.PONDER_RPC_URL_1 },
+    { name: 'mainnet', chainId: 1, rpcUrl: process.env.PONDER_RPC_URL_1 },
   ],
   contracts: [
     {
-      name: "DNSRegistrar",
-      network: "mainnet",
-      abi: "./abis/DNSRegistrar.json",
-      address: "0x58774Bb8acD458A640aF0B88238369A167546ef2",
+      name: 'DNSRegistrar',
+      network: 'mainnet',
+      abi: './abis/DNSRegistrar.json',
+      address: '0x58774Bb8acD458A640aF0B88238369A167546ef2',
       startBlock: 13040372,
     },
+    {
+      name: 'DNSRegistrarOld',
+      network: 'mainnet',
+      abi: './abis/DNSRegistrar.json',
+      address: '0xa2F428617a523837d4adC81C67a296d42FD95e86',
+      startBlock: 9380528,
+    },
   ],
-};
+}
--- src/DNSRegistrar.ts
+++ src/DNSRegistrar.ts
@@ -14,3 +14,18 @@ ponder.on('DNSRegistrar:Claim', async ({ event, context }) => {
     update: { name },
   })
 })
+
+ponder.on('DNSRegistrarOld:Claim', async ({ event, context }) => {
+  const { DnsName } = context.entities
+  const { dnsname: encodedName } = event.params
+
+  const buffer = Buffer.from(encodedName.split('0x')[1], 'hex')
+  const _name = buffer.toString('utf8').replace(/[\x01-\x20]/g, '.')
+  const name = _name.slice(1, _name.length - 1)
+
+  await DnsName.upsert({
+    id: encodedName,
+    create: { name },
+    update: { name },
+  })
+})
